45a6574e9c04f72641e93637393d437045077196
Dan <dseventeen@gmail.com>
2019-10-31 19:13:18 +0000

    both lists solved

--- list1.rb
+++ list1.rb
@@ -1,34 +1,56 @@
 class LinkedListNode
   attr_accessor :value, :next_node
 
-  def initialize(value, next_node=nil)
+  def initialize(value, next_node= nil)
     @value = value
     @next_node = next_node
   end
 end
-
-
+ 
 def print_values(list_node)
   if list_node
     print "#{list_node.value} --> "
     print_values(list_node.next_node)
+ 
   else
     print "nil\n"
     return
+
   end
 end
 
-def reverse_list(list, previous = nil)
-      current_head = list.next_node
-      list.next_node = previous
-      
-        if current_head
-        reverse_list(current_head, list)
-      else
-        list
-       end
+class Stack
+  attr_reader :data
+
+  def initialize
+    @data = nil
+  end
+
+  def push(value)  
+    @data = LinkedListNode.new(value, @data)
+  end
+
+  def pop 
+    
+    while @data != nil
+      value = @data.next_node
+     
+      return value
+
+    end
+  end 
 end
 
+def reverse_list(list)
+  stack = Stack.new
+
+  while (list != nil)
+    stack.push(list.value)
+    list = list.next_node
+  
+  end
+    stack.data
+end
 
 node1 = LinkedListNode.new(37)
 node2 = LinkedListNode.new(99, node1)
